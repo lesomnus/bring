051e8abc4b40a5aac203fad63c3adaf8cd10bc0c
Seunghyun Hwang <lesomnus@gmail.com>
2024-11-26 23:07:42 +0900

    handle error on the caller

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"context"
+	"errors"
+	"log/slog"
 	"os"
 
 	"github.com/lesomnus/bring/cmd"
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	app := cmd.NewApp()
-	app.Run(context.Background(), os.Args)
+	var l *slog.Logger
+	handleError := func(err error) {
+		l.Error(err.Error())
+
+		var e cli.ExitCoder
+		if errors.As(err, &e) {
+			os.Exit(e.ExitCode())
+		} else {
+			os.Exit(1)
+		}
+	}
+
+	app := cmd.NewApp(&l)
+	app.ExitErrHandler = func(ctx context.Context, cmd *cli.Command, err error) {
+		handleError(err)
+	}
+	if err := app.Run(context.Background(), os.Args); err != nil {
+		handleError(err)
+	}
 }
